b97cc6a5be328daf067a90f7cab8d7854ede6b1b
hyojung <hyojunghan1016@gmail.com>
2017-05-15 21:19:15 +0900

    init file

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,14 @@ from . import function
 app = Flask(__name__)
 from app import views
 
-function.Make_db_member()
-function.Make_db_house()
-function.Make_db_pet()
-function.Make_db_petsitter()
-function.Make_db_image()
-function.Make_db_tran()
 
-views.loop_insert()
+try:
+    function.Make_db_member()
+    function.Make_db_house()
+    function.Make_db_pet()
+    function.Make_db_petsitter()
+    function.Make_db_image()
+    function.Make_db_tran()
+    views.loop_insert()
+except:
+    print("db is already existed")
